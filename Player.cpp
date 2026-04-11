e60f2ed6504467991d529cc032082ced24c6a365
Tingwei xie <xiett001@mymail.unisa.edu.au>
2026-04-11 23:32:38 +0930

    Added some more basic Player methods. The player is now able to obtain a random name and save drawn cards in the playing field.

--- Player.cpp
+++ Player.cpp
@@ -8,13 +8,34 @@ Player::Player()
     name = "";
 }
 
-//void Player::setRandomName()
-//{
-//    std::string names[] = { "Sam", "Billy", "Jen", "Bob", "Sally",
-//                            "Joe", "Sue", "Sasha", "Tina", "Marge" };
-//
-//    name = names[rand() % 10];
-//}
-//
-//std::string Player::getName() const
-//{
+void Player::setRandomName()
+{
+    std::string names[] = { "Sam", "Billy", "Jen", "Bob", "Sally",
+                            "Joe", "Sue", "Sasha", "Tina", "Marge" };
+
+    name = names[rand() % 10];
+}
+
+std::string Player::getName() const
+{
+    return name;
+}
+
+void Player::addToPlayArea(Card* card)
+{
+    playArea.push_back(card);
+}
+
+void Player::printCollection(const CardCollection& cards) const
+{
+}
+
+void Player::printPlayArea() const
+{
+    printCollection(playArea);
+}
+
+void Player::printBank() const
+{
+    printCollection(bank);
+}
