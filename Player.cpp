b57f32c1daa0f4626dcc50374d61a13993e4c07c
Tingwei xie <xiett001@mymail.unisa.edu.au>
2026-04-12 00:05:15 +0930

    Coded the logic of dealing with the Player cards. The player will now be able to verify whether there are duplicate suits on the play area and initiate the rule of bust when a card is played.

--- Player.cpp
+++ Player.cpp
@@ -21,6 +21,33 @@ std::string Player::getName() const
     return name;
 }
 
+bool Player::hasSuitInPlayArea(CardType type) const
+{
+
+    for(int i = 0; i < playArea.size(); i++)
+    {
+        if (playArea[i]->type() == type)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Player::playCard(Card* card, Game& game)
+{
+    if (hasSuitInPlayArea(card->type()))
+    {
+        addToPlayArea(card);
+        return true;
+    }
+
+    addToPlayArea(card);
+    card->play(game, *this);
+    return false;
+}
+
+
 void Player::addToPlayArea(Card* card)
 {
     playArea.push_back(card);
