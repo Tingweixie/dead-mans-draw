ff71cf4e32f414e0cbb561f87932776ab900e471
Tingwei xie <xiett001@mymail.unisa.edu.au>
2026-04-11 23:23:14 +0930

    Started the Player class and added the basic player data and it prepares the name of each player, playing field and bank.

---
+++ Player.cpp
@@ -0,0 +1,20 @@
+#include "Player.h"
+
+#include <cstdlib>
+#include <iostream>
+
+Player::Player()
+{
+    name = "";
+}
+
+//void Player::setRandomName()
+//{
+//    std::string names[] = { "Sam", "Billy", "Jen", "Bob", "Sally",
+//                            "Joe", "Sue", "Sasha", "Tina", "Marge" };
+//
+//    name = names[rand() % 10];
+//}
+//
+//std::string Player::getName() const
+//{
---
+++ Player.h
@@ -0,0 +1,38 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <string>
+#include "Card.h"
+
+class Game;
+
+class Player
+{
+private:
+    std::string name;
+    CardCollection playArea;
+    CardCollection bank;
+
+    void printCollection(const CardCollection& cards) const;
+
+public:
+    Player();
+
+    void setRandomName();
+    std::string getName() const;
+
+    bool playCard(Card* card, Game& game);
+    bool hasSuitInPlayArea(CardType type) const;
+    void addToPlayArea(Card* card);
+
+    void bankPlayArea(Game& game);
+    void discardPlayArea(Game& game);
+
+    void printPlayArea() const;
+    void printBank() const;
+
+    int getScore() const;
+    Card* removeHighestBankCard(CardType type);
+};
+
+#endif
